ab32f79aeb389ce4abf880c150efca1b02b6a40e
nerv-bit <namsjeev@gmail.com>
2026-01-07 15:10:48 -0800

    Create next.config.js

---
+++ app/next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [
+    "@tsparticles/react",
+    "@tsparticles/slim",
+    "tsparticles-engine"
+  ],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false, // Ignores node-only 'canvas' module during build
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
