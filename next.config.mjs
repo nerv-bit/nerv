40e1f5398785aacbb658c656a929346e9dc8c9ac
nerv-bit <namsjeev@gmail.com>
2026-01-07 15:49:55 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,11 @@ const nextConfig = {
     "@tsparticles/react",
     "@tsparticles/slim",
     "@tsparticles/engine",
-    "tsparticles-engine"
   ],
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       canvas: false,
-      fs: false, // Optional: add if you see fs-related errors
     };
     return config;
   },
