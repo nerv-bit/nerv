d3a4f4ec1892fbed8b55c67ab25fd20daf3d5896
nerv-bit <namsjeev@gmail.com>
2026-01-07 15:32:19 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,14 @@ const nextConfig = {
   transpilePackages: [
     "@tsparticles/react",
     "@tsparticles/slim",
+    "@tsparticles/engine",
     "tsparticles-engine"
   ],
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      canvas: false, // Ignores node-only 'canvas' module during build
+      canvas: false,
+      fs: false, // Optional: add if you see fs-related errors
     };
     return config;
   },
