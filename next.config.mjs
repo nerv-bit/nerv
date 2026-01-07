5e30070c853a8475433dd86be6f886b234088b65
nerv-bit <namsjeev@gmail.com>
2026-01-07 15:26:40 -0800

    Add tsparticles-engine

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: [
+    "@tsparticles/react",
+    "@tsparticles/slim",
+    "tsparticles-engine"
+  ],
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false, // Ignores node-only 'canvas' module during build
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
