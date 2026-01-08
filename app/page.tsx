109ec834a96005516df88150f09f4cdb59bff264
nerv-bit <namsjeev@gmail.com>
2026-01-07 18:09:37 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import dynamic from 'next/dynamic';
-import { motion } from 'framer-motion';
+import { animated, useSpring, config } from '@react-spring/web';
 import Image from 'next/image';
 
 const ParticlesComponent = dynamic(() => import('./ParticlesComponent'), {
